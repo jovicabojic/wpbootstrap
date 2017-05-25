197a5a1ae83f5ce7ff96f0f9b6bd3f1cca3683bd
jovicabojic <bojic.jovica@hotmail.com>
2017-05-26 01:32:15 +0200

    Update javascript version

--- wpbootstrap-wp/footer.php
+++ wpbootstrap-wp/footer.php
@@ -16,7 +16,7 @@
 </footer>
 <?php wp_footer(); ?>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.js"></script>
 </body>
 </html>
